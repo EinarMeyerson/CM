5bbf8f75d601c0baf3eda833dd8713fea359d55e
José Mª Galisteo <Jos M Galisteo@MyPC>
2014-12-08 13:12:47 +0100

    orden de imports

--- src/com/tfd/classmarks/ListAdapter.java
+++ src/com/tfd/classmarks/ListAdapter.java
@@ -9,8 +9,6 @@ import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
